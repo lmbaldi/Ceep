a6af44d8f3f52a167a690644dc82b16549ca0567
lmbadi <luizmarcelobaldi@hotmail.com>
2020-12-03 20:24:22 -0300

    first commit

---
+++ app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,16 @@
+package com.example.ceep.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ceep.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
