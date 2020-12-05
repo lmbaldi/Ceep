cda801f9f9002e8763747e9b2ce292d5ffdcb36a
lmbadi <luizmarcelobaldi@hotmail.com>
2020-12-04 21:28:58 -0300

    add model, dao,adapter

--- app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
@@ -3,8 +3,15 @@ package com.example.ceep.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.example.ceep.R;
+import com.example.ceep.dao.NotaDAO;
+import com.example.ceep.model.Nota;
+import com.example.ceep.ui.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +19,13 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+        NotaDAO dao = new NotaDAO();
+        for(int i = 1; i<= 100; i++){
+            dao.insere(new Nota("Primeira Nota " + i, "Primeira descricao " + i));
+        }
+        List<Nota> todasNotas = dao.todos();
+        listaNotas.setAdapter(new ListaNotasAdapter(this,todasNotas));
     }
 }
